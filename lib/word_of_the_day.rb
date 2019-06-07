baacade305e9c88942b83117c5995661d24ebd81
Abagail Hudson <abagailanne@gmail.com>
2019-06-06 20:44:40 -0400

    first commit

---
+++ lib/word_of_the_day.rb
@@ -0,0 +1,6 @@
+require "word_of_the_day/version"
+
+module WordOfTheDay
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/word_of_the_day_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe WordOfTheDay do
+  it "has a version number" do
+    expect(WordOfTheDay::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
